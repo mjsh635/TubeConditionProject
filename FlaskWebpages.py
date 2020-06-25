4a24e692ad88bb87e1685846f0dc60cbec9fb011
MSH <mjsh635@gmail.com>
2020-06-25 07:57:40 -0400

    added flash(), this shows a message on the next request

--- FlaskWebpages.py
+++ FlaskWebpages.py
@@ -3,10 +3,14 @@ that will start/stop conditioning, and configure the power supplies
 to the correct values for the type of conditioning.
 """
 
-from flask import Flask, render_template, request, redirect, send_file, send_from_directory
+from flask import Flask, render_template, request, redirect, send_file, send_from_directory, flash
 import time,datetime
 import os
+from Scripts import Raspberry_PI_IO_Controller
+
+RPI = Raspberry_PI_IO_Controller.RPIO()
 app = Flask(__name__)
+app.secret_key = 'random string'
 
 """ When connecting to landing page "/" redirect
 to the Home page"""
@@ -23,14 +27,13 @@ def add_header(response):
 def redirect_to_main_page():
     return redirect("/Quick_Access")
 
-##reminder to change the fixed values to read previous sets
 @app.route("/Quick_Access", methods=["GET", "POST"])
 def Home_Load():
-    return render_template("QuickAccess.html", CurrKV = 1, CurrMA = 2)
+    return render_template("QuickAccess.html", CurrKV = RPI.currKV, CurrMA = RPI.currMA)
 
 @app.route("/hvsupplypage", methods=["GET", "POST"])
 def HVSettings():
-    return render_template("HVSettings.html")
+    return render_template("HVSettings.html",condKVStart = RPI.condKVStart, condKVTarget = RPI.condKVTarget,condMAStart = RPI.condMAStart, condMATarget =RPI.condMATarget,condStepDwell=RPI.condStepDwell, CondPostArcDwell = RPI.CondPostArcDwell,CondOffDwell = RPI.CondOffDwell,CondStepCount = RPI.CondStepCount)
 
 @app.route("/LogFileDownloaderPSU1", methods = ['GET'])
 def downloadLog():
@@ -40,17 +43,29 @@ def downloadLog():
 @app.route("/ManualXrayControl", methods = ["POST"])
 def XrayONOFF():
     if "Xray_On" in request.form.keys():
-        print("Xray turning On to: ", request.form["kvSet"], " and ", request.form["mASet"] )
+        compresp = RPI.XrayOn(request.form["kvSet"], request.form["mASet"])
 
     elif "Xray_Off" in request.form.keys():
-        print("Xray turing off")
+        compresp = RPI.XrayOff()
     
-
+    elif "CurrentValues" in request.form.keys():
+        compresp = RPI.currentKVMA()
+    flash(compresp)
     return redirect("/Quick_Access")
 
+@app.route("/updateHV", methods = ["POST", "GET"])
+def HVUpdate():
+    msg = ""
+    if request.method == "POST":
+        msg = RPI.updateHVSettings(request.form["condKVStart"], request.form["condKVTarget"],request.form["condMAStart"], request.form["condMATarget"], request.form["condStepDwell"],request.form["CondPostArcDwell"], request.form["CondOffDwell"], request.form["CondStepCount"])
+
+    if request.method == "GET":
+        print("Updating time calculation")
+
+    flash(msg)
+    return redirect("/hvsupplypage")
 
 import socket
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 current_IP = s.getsockname()[0]
