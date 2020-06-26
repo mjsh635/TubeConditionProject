ba56ee3561b05eeac3028904f3dc23efb3990219
MSH <mjsh635@gmail.com>
2020-06-26 08:20:14 -0400

    remove unused code

--- Scripts/Raspberry_PI_IO_Controller.py
+++ Scripts/Raspberry_PI_IO_Controller.py
@@ -34,41 +34,6 @@ class RPIO():
         self.CondPostArcDwell = settings["CondPostArcDwell"]
         self.CondOffDwell = settings["CondOffDwell"]
         self.CondStepCount = settings["CondStepCount"]
-        
-    # import board
-    # import busio
-    # import adafruit_ads1x15.ads1015 as ADS
-    # from adafruit_ads1x15.analog_in import AnalogIn
-
-    # # Create the I2C bus
-    # i2c = busio.I2C(board.SCL, board.SDA)
-    # dev1_addres = 0x00
-    # dev2_addres = 0x00
-    # dev3_addres = 0x00
-    # # Create the ADC object using the I2C bus
-    # ADC_Device1 = ADS.ADS1015(i2c,dev1_addres)
-    # # ADC_Device2 = ADS.ADS1015(i2c, dev2_addres)
-    # # ADC_Device3 = ADS.ADS1015(i2c, dev3_addres)
-
-    # Dev1chan0 = AnalogIn(ADC_Device1, ADS.P0)
-    # Dev1chan1 = AnalogIn(ADC_Device1, ADS.P1)
-    # Dev1chan2 = AnalogIn(ADC_Device1, ADS.P2)
-    # Dev1chan3 = AnalogIn(ADC_Device1, ADS.P3)
-
-    # # Dev2chan0 = AnalogIn(ADC_Device2, ADS.P0)
-    # # Dev2chan1 = AnalogIn(ADC_Device2, ADS.P1)
-    # # Dev2chan2 = AnalogIn(ADC_Device2, ADS.P2)
-    # # Dev2chan3 = AnalogIn(ADC_Device2, ADS.P3)
-
-    # # Dev3chan0 = AnalogIn(ADC_Device3, ADS.P0)
-    # # Dev3chan1 = AnalogIn(ADC_Device3, ADS.P1)
-    # # Dev3chan2 = AnalogIn(ADC_Device3, ADS.P2)
-    # # Dev3chan3 = AnalogIn(ADC_Device3, ADS.P3)
-
-    # def readValue( channel):
-    #     channel_data = [channel.value, channel.voltage]
-    #     return channel_data
-
         self.readKV = 0.0
         self.readMA = 0.0
         self.set_GPIO(17,1)
