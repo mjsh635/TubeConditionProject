87ceb0e75561c50cb0510640c4a62c773eccfd2f
MSH <mjsh635@gmail.com>
2020-07-01 08:26:26 -0400

    Start of unittesting code

---
+++ UnitTesting.py
@@ -0,0 +1,8 @@
+"""
+This will be the start of the unit testing file.
+this will use the "unittest" python library
+
+https://docs.python.org/3/library/unittest.html#module-unittest
+
+"""
+import unittest
