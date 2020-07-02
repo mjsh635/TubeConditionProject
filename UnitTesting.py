617116ffa1b90c13f83cbff5d65137da43e95981
MSH <mjsh635@gmail.com>
2020-07-02 12:12:31 -0400

    deleting unneeded files

--- UnitTesting.py
+++
@@ -1,8 +0,0 @@
-"""
-This will be the start of the unit testing file.
-this will use the "unittest" python library
-
-https://docs.python.org/3/library/unittest.html#module-unittest
-
-"""
-import unittest
