b84a648645bc2b08f0cb3dbfa8c785322daf1099
MSH <mjsh635@gmail.com>
2020-07-16 09:40:48 -0400

    logger searches directory now, and can zip files

--- MyScripts/Logging_Controller.py
+++ MyScripts/Logging_Controller.py
@@ -34,4 +34,31 @@ class Conditioning_Logger():
         """
         with open(self.filepath, mode="a") as OpenedLogFile:
             if OpenedLogFile.writable():
-                OpenedLogFile.writelines(log_data + "\n")
+                OpenedLogFile.writelines(log_data + "\n")                OpenedLogFile.writelines(f"{log_data}\n")
+
+    def search_directory(self):
+        """ Search path and return list of files 
+        
+        :returns: (list) returns list of files in constructors directory
+        """
+        files_in_dir = []
+
+        # r=>root, d=>directories, f=>files
+        for r, d, f in os.walk(self.folder_path):
+            for item in f:
+                if '.txt' in item:
+                    files_in_dir.append(os.path.join(r, item))
+        return files_in_dir
+
+    def zip_files(self, foldername, files):
+        """take all files return a zip'd folder containing them
+        
+        :param files: (list) files to be zipped
+        
+        :param foldername: (str) name of zip'd folder
+        """
+        with ZipFile((f"{self.folder_path}\\{foldername}.zip"), 'w') as zip:
+            for txtfile in files:
+                zip.write(txtfile)
+        return f"{self.folder_path}\\{foldername}.zip"
+
