15335f709a297c6341a0d302f3e42e08a63ae96b
MSH <mjsh635@gmail.com>
2020-07-16 09:39:39 -0400

    changes to autoconfiguring data. using supply model number now

--- MyScripts/DXM.py
+++ MyScripts/DXM.py
@@ -10,7 +10,7 @@ class DXM_Supply:
         self.connect()
         self.remote_mode()
         raw_model = self.read_model_type()
-        self.model = raw_model[2]
+        self.model = raw_model[1]
         self.disconnect()
         
 
@@ -33,28 +33,28 @@ class DXM_Supply:
 
     def read_voltage_out(self):
         response = self.__send_command(19, '')
-        if self.model == 'DXM33':
+        if self.model == 'X4087':
             scaled_voltage = float(response[1]) * 0.00976
-        elif self.model == 'DXM02':
+        elif self.model == 'X3481':
             scaled_voltage = float(response[1]) * 0.007326007
-        elif self.model == 'DXM21':
+        elif self.model == 'X4911':
             scaled_voltage = float(response[1]) * 0.00976
-        elif self.model == 'DXM20':
+        elif self.model == 'X4313':
             scaled_voltage = float(response[1]) * 0.007326007
         return scaled_voltage
 
     def read_model_type(self):
-        return self.__send_command(26,'')
+        return self.__send_command(26, '')
 
     def read_current_out(self):
         response = self.__send_command(19, '')
-        if self.model == 'DXM33':
+        if self.model == 'X4087':
             scaled_current = float(response[2]) * 0.007326
-        elif self.model == 'DXM02':
+        elif self.model == 'X3481':
             scaled_current = float(response[2]) * 0.002442002
-        elif self.model == 'DXM21':
+        elif self.model == 'X4911':
             scaled_current = float(response[2]) * 0.00366300
-        elif self.model == 'DXM20':
+        elif self.model == 'X4313':
             scaled_current = float(response[2]) * 0.00488400
         return scaled_current
 
@@ -66,25 +66,25 @@ class DXM_Supply:
 
     def request_voltage_set(self):
         response = self.__send_command(14, '')
-        if self.model == 'DXM33':
+        if self.model == 'X4087':
             scaled_voltage = float(response[1]) * 0.00976
-        elif self.model == 'DXM02':
+        elif self.model == 'X3481':
             scaled_voltage = float(response[1]) * 0.007326007
-        elif self.model == 'DXM21':
+        elif self.model == 'X4911':
             scaled_voltage = float(response[1]) * 0.00976
-        elif self.model == 'DXM20':
+        elif self.model == 'X4313':
             scaled_voltage = float(response[1]) * 0.007326007
         return scaled_voltage
 
     def request_current_set(self):
         response = self.__send_command(15, '')
-        if self.model == 'DXM33':
+        if self.model == 'X4087':
             scaled_current = float(response[2]) * 0.007326
-        elif self.model == 'DXM02':
+        elif self.model == 'X3481':
             scaled_current = float(response[2]) * 0.002442002
-        elif self.model == 'DXM21':
+        elif self.model == 'X4911':
             scaled_current = float(response[2]) * 0.00366300
-        elif self.model == 'DXM20':
+        elif self.model == 'X4313':
             scaled_current = float(response[2]) * 0.00488400
         return scaled_current
 
@@ -104,13 +104,13 @@ class DXM_Supply:
         """
         :param voltage_to_set: (float) 0-40
         """
-        if self.model == 'DXM33':
+        if self.model == 'X4087':
             raw_voltage_to_set = math.trunc(float(voltage_to_set) / 0.00976)
-        elif self.model == 'DXM02':
+        elif self.model == 'X3481':
             raw_voltage_to_set = math.trunc(float(voltage_to_set) / 0.007326007)
-        elif self.model == 'DXM21':
+        elif self.model == 'X4911':
             raw_voltage_to_set = math.trunc(float(voltage_to_set) / 0.00976)
-        elif self.model == 'DXM20':
+        elif self.model == 'X4313':
             raw_voltage_to_set = math.trunc(float(voltage_to_set) / 0.007326007)
         return self.__send_command(10, raw_voltage_to_set)
 
@@ -118,13 +118,13 @@ class DXM_Supply:
         """
         :param current_to_set: (float) 0-30
         """
-        if self.model == 'DXM33':
+        if self.model == 'X4087':
             raw_current_to_set = math.trunc(float(current_to_set) / .007326)
-        elif self.model == 'DXM02':
+        elif self.model == 'X3481':
             raw_current_to_set = math.trunc(float(current_to_set) / 0.002442002)
-        elif self.model == 'DXM21':
+        elif self.model == 'X4911':
             raw_current_to_set = math.trunc(float(current_to_set) / 0.00366300)
-        elif self.model == 'DXM20':
+        elif self.model == 'X4313':
             raw_current_to_set = math.trunc(float(current_to_set) / 0.00488400)
 
         return self.__send_command(11, raw_current_to_set)
